7d56c1b8fbc560097985805bddde6a971a48d04e
Tore Mørkved <tom@Tores-DIPS-Mac.local>
2020-05-26 08:50:59 +0200

    init

---
+++ pocwardlist/PatientListService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Bogus;
+
+namespace pocwardlist
+{
+    public static class PatientListService
+    {
+        public static ObservableCollection<ColumnGroup> CreateColumns()
+        {
+            var groups = new ObservableCollection<ColumnGroup>();
+            groups.Add(new ColumnGroup("TopLeft", "TL"));
+            groups.Add(new ColumnGroup("TopRight", "TR"));
+            groups.Add(new ColumnGroup("Left", "M"));
+            groups.Add(new ColumnGroup("Right", "S"));
+            foreach (var group in groups)
+            {
+                for (var i = 0; i < 5; i++)
+                {
+                    group.Add(new Column
+                    {
+                        Header = "Header " + i,
+                        Value = "Value " + i,
+                        Group = group.Title
+                    });
+                }
+            }
+
+            return groups;
+        }
+
+        public static ObservableCollection<Patient> GetPatients()
+        {
+            var patientSetup = new Faker<Patient>()
+
+                .RuleFor(u => u.Gender, f => f.PickRandomParam("Male", "Female"))
+                .RuleFor(u => u.Name, (f, u) => f.Name.FullName((Bogus.DataSets.Name.Gender)Enum.Parse(typeof(Bogus.DataSets.Name.Gender), u.Gender)))
+                .RuleFor(u => u.SSN, (f, u) => f.Random.Replace("######-####"))
+                .RuleFor(u => u.Age, (f, u) => f.Random.Number(100))
+                ;
+
+            var fakePatients = patientSetup.Generate(10);
+            var patients = new ObservableCollection<Patient>(fakePatients);
+            return patients;
+        }
+    }
+}
---
+++ pocwardlist/SettingsPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace pocwardlist
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsViewModel vm { get; set; }
+        public SettingsPage(ObservableCollection<ColumnGroup> columns)
+        {
+            vm = new SettingsViewModel(columns);
+            BindingContext = vm;
+            InitializeComponent();
+        }
+
+
+        async void ToolbarItem_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+
+    public class SettingsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<ColumnGroup> ColumnGroups { get; set; }
+        
+        public SettingsViewModel(ObservableCollection<ColumnGroup> groups)
+        {
+            ColumnGroups = groups;
+        }
+    }
+
+    public class ColumnGroup : List<Column>
+    {
+        public string Title { get; set; }
+        public string ShortName { get; set; } //will be used for jump lists
+        public string Subtitle { get; set; }
+        public ColumnGroup(string title, string shortName)
+        {
+            Title = title;
+            ShortName = shortName;
+        }
+
+    }
+    public class Column : INotifyPropertyChanged
+    {
+        public string Header { get; set; }
+        public string Value { get; set; }
+        public string Group { get; set; }
+        public bool Show { get; set; }
+        public Color StatusColor { get; set; }
+        public Command Tapped { get; set; }
+
+        public Column()
+        {
+            Tapped = new Command(() => {
+                Show = !Show;
+                StatusColor = Show ? Color.LightGreen : Color.White;
+
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
